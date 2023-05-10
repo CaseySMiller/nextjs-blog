ae8d81b45308a847fb5b4d6b17fa1c382dde8df5
CaseySMiller <CaseySMiller@yahoo.com>
2023-05-10 13:50:31 -0600

    completed assets, metadata and css section

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,28 @@
-import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">return to home page</Link>
-      </h2>
-      
+      <Layout>
+        <Head>
+        <title>First Post</title>
+        </Head>
+        
+        <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+        />
+
+        <h1>First Post</h1>
+        <h2>
+          This is a post. It posts stuff
+        </h2>
+      </Layout>
     </>
   );
 }
